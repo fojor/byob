29b1e7b300d0651c87370e761bf70ee799906cf6
Aleksey Bober <aleksey.bober@gmail.com>
2019-10-24 11:00:11 +0300

    slides

--- src/app/components/board/drawio/drawio.component.ts
+++ src/app/components/board/drawio/drawio.component.ts
@@ -13,7 +13,7 @@ import { Component, ViewChild, ElementRef, OnInit, ViewEncapsulation, OnDestroy,
     encapsulation: ViewEncapsulation.None
 })
 export class DrawIOComponent implements OnDestroy {
-    private _url = 'drawio/index.html?embed=1&spin=1&proto=json&configure=1&pv=0&sb=0&vrs=4';
+    private _url = 'drawio/index.html?embed=1&pages=1&proto=json&configure=1&sb=0&vrs=7';
     //'https://www.draw.io/?embed=1&spin=1&proto=json&configure=1';
     private _data: string;
 
@@ -109,9 +109,20 @@ export class DrawIOComponent implements OnDestroy {
             autosave: 1, 
         }
 
-        if(xml) {
-            options.xml = xml;
-        }
+        //if(xml) {
+            options.xml = xml
+                || 
+                `<mxfile modified="2019-10-22T06:13:05.335Z" host="localhost" agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" version="11.1.2" etag="AkylOAzUnc7xuP1y37lp" compressed="false">
+                    <diagram id="U3r4llsrBNhhFNVDR43Q">
+                        <mxGraphModel grid="0" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" math="0" shadow="0">
+                            <root>
+                                <mxCell id="0"/>
+                                <mxCell id="1" parent="0"/>
+                            </root>
+                        </mxGraphModel>
+                    </diagram>
+                </mxfile>`
+        //}
 
         this.iframe.contentWindow.postMessage(JSON.stringify(options), '*');
     }
--- src/drawio/custom.js
+++ src/drawio/custom.js
@@ -1,14 +1,53 @@
-
-var actionsInit = Actions.prototype.init;
-Actions.prototype.init = function()
-{
-    actionsInit.apply(this, arguments); 
+function setDefaultContent (actions, setData) {
+    var ui = actions.editorUi;
+    var editor = ui.editor;
+    var graph = editor.graph;
 
     setTimeout(() => {
-        //disable grid by default
-        this.get('grid').funct();
-    }, 200);
-}
+        if(setData) {
+            var data = `<mxfile modified="2019-10-22T06:13:05.335Z" host="localhost" agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" version="11.1.2" etag="AkylOAzUnc7xuP1y37lp" compressed="false">
+                            <diagram id="U3r4llsrBNhhFNVDR43Q">
+                                <mxGraphModel grid="0" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" math="0" shadow="0">
+                                    <root>
+                                        <mxCell id="0"/>
+                                        <mxCell id="1" parent="0"/>
+                                    </root>
+                                </mxGraphModel>
+                            </diagram>
+                        </mxfile>`;
+            ui.editor.graph.model.beginUpdate();
+            try
+            {
+                ui.editor.setGraphXml(mxUtils.parseXml(data).documentElement);
+            }
+            catch (e)
+            {
+                error = e;
+            }
+            finally
+            {
+                ui.editor.graph.model.endUpdate();				
+            }
+        }
+        
+        
+        if (!graph.pageVisible)
+        {
+            actions.get('pageView').funct();
+        }
+    
+        actions.get('resetView').funct();
+    }, 100); 
+};
+
+var editorUiInsertPage = EditorUi.prototype.insertPage;
+EditorUi.prototype.insertPage = function(page, index)
+{
+    var page = editorUiInsertPage.apply(this, arguments); 
+    this.actions.get('grid').funct();
+    setDefaultContent(this.actions, true);	
+	return page;
+};
 
 var editorUiInit = EditorUi.prototype.init;
 EditorUi.prototype.init = function()
@@ -120,7 +159,7 @@ Sidebar.prototype.createDropHandler = function(cells, allowSplit, allowCellsInse
     
 
     if(cells[0].value === 'Text' && urlParams['new'] === '1') {
-        //console.log('autocreate text')
+        setDefaultContent(this.editorUi.actions);
         var graph = this.editorUi.editor.graph;
         setTimeout(() => {
             //var cell = graph.addText(30, 20, null);
@@ -240,16 +279,129 @@ Sidebar.prototype.createDropHandler = function(cells, allowSplit, allowCellsInse
 	});
 };
 
-
-EditorUi.prototype.resetScrollbars = function()
+EditorUi.prototype.updateTabContainer = function()
 {
-	var graph = this.editor.graph;
-	
-    graph.container.scrollTop = 0;
-    graph.container.scrollLeft = 0;
+	if (this.tabContainer && this.pages && this.pages.length)
+	{
+        this.tabContainer.style.height = '130px';
+        this.tabContainer.style.overflowX = 'scroll';
+        this.tabContainer.style.overflowY = 'hidden';
 
-    if (!mxUtils.hasScrollbars(graph.container))
-    {
-        graph.view.setTranslate(0, 0);
+		var wrapper = document.createElement('div');
+		wrapper.style.position = 'relative';
+		wrapper.style.display = (mxClient.IS_QUIRKS) ? 'inline' : 'inline-block';
+		wrapper.style.verticalAlign = 'top';
+        wrapper.style.whiteSpace = 'nowrap';
+        wrapper.style.overflowX = 'srcoll';
+		wrapper.style.overflow = 'hidden';
+		wrapper.style.fontSize = '13px';
+        wrapper.style.marginLeft = '10px';
+		
+        var tabWidth = 160;
+        wrapper.style.width = (tabWidth + 8) * (this.pages.length + 1) + 'px';
+
+		var startIndex = null;
+
+		for (var i = 0; i < this.pages.length; i++)
+		{
+			(mxUtils.bind(this, function(index, tab) {
+                tab.innerHTML = '';
+
+                if (this.pages[index] == this.currentPage)
+				{
+					tab.className = 'geActivePage';
+				}
+				else
+				{
+					tab.className = 'geInactivePage';
+				}
+
+                try {
+                    var diagramNode = this.pages[index].node;
+                    var xml = Graph.decompress(mxUtils.getTextContent(diagramNode));
+                    var xmlDoc = mxUtils.parseXml(xml);
+                    var graph = new Graph(tab);
+                    var codec = new mxCodec(xmlDoc);
+                    codec.decode(xmlDoc.documentElement, graph.getModel());
+                    graph.foldingEnabled = false;
+                    graph.setEnabled(false);
+                    graph.view.setScale(0.15);      
+                }
+                catch {
+                }                     
+			
+                tab.setAttribute('draggable', 'true');
+                tab.removeAttribute('title');
+				mxEvent.addListener(tab, 'dragstart', mxUtils.bind(this, function(evt)
+				{
+                    startIndex = index;
+				}));
+				
+				mxEvent.addListener(tab, 'dragend', mxUtils.bind(this, function(evt)
+				{
+					startIndex = null;
+					evt.stopPropagation();
+					evt.preventDefault();
+				}));
+				
+				mxEvent.addListener(tab, 'dragover', mxUtils.bind(this, function(evt)
+				{
+					if (startIndex != null)
+					{
+						evt.dataTransfer.dropEffect = 'move';
+					}
+					
+					evt.stopPropagation();
+					evt.preventDefault();
+				}));
+				
+				mxEvent.addListener(tab, 'drop', mxUtils.bind(this, function(evt)
+				{
+					if (startIndex != null && index != startIndex)
+					{
+						this.movePage(startIndex, index);
+					}
+
+					evt.stopPropagation();
+					evt.preventDefault();
+				}));
+                
+				wrapper.appendChild(tab);
+			}))(i, this.createTabForPage(this.pages[i], tabWidth, this.pages[i] != this.currentPage, i + 1));
+        }
+        
+        var insertTab = this.createPageInsertTab();
+        insertTab.className = "insert-tab";
+        insertTab.style.width = tabWidth + 'px';
+        insertTab.style.lineHeight = this.tabContainer.style.height;
+        wrapper.appendChild(insertTab);
+		
+        this.tabContainer.innerHTML = '';
+        this.tabContainer.appendChild(wrapper);
     }
+    
+};
+
+var editorUIcreateTabContainer = EditorUi.prototype.createTabContainer    
+EditorUi.prototype.createTabContainer = function()
+{
+	setTimeout(() => {
+        var tabContainer = document.querySelector('.geTabContainer');
+        tabContainer.scrollTo(9999999, 0);
+    }, 1000);
+
+	var container = editorUIcreateTabContainer.apply(this, arguments);
+	return container;
+};
+
+var editorUIinsertPage = EditorUi.prototype.insertPage    
+EditorUi.prototype.insertPage = function(page, index)
+{
+    setTimeout(() => {
+        var tabContainer = document.querySelector('.geTabContainer');
+        tabContainer.scrollTo(9999999, 0);
+    }, 50);
+    
+    var page = editorUIinsertPage.apply(this, arguments);
+	return page;
 };
